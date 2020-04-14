be0f0761db97e801eae277bb4de0d1ba31d6ecf7
LazyHacker <github@lazyhacker.com>
2020-04-13 21:26:49 -0700

    vanity import

--- internal/yt/youtube.go
+++ internal/yt/youtube.go
@@ -1,6 +1,6 @@
 // Package yt is a wrapper around the Youtube Data API for creating new
 // playlists, add videos to a playlist and subscribing to channels.
-package yt
+package yt // import "lazyhacker.dev/youtube-copy/internal-yt"
 
 import (
 	"encoding/json"
